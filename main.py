bc98373c66e0734dcd5de7581d01e25929321d1d
Rido Atmanto <ridoatmanto@gmail.com>
2021-06-26 14:46:53 +0700

    Add two class sample output

---
+++ main.py
@@ -0,0 +1,12 @@
+from geometri.rectangle import Rectangle
+from geometri.triangle import Triangle
+
+rectangle = Rectangle(8, 4)
+print(rectangle.info())
+
+print(f'Rectnngle Count Area is {rectangle.count_area()}')
+
+triangle = Triangle(7, 10)
+print(triangle.info())
+
+print(f'Triangle Count Area is {triangle.count_area()}')
