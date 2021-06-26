e537d06431b3677493473786d302eea43e0f447d
Rido Atmanto <ridoatmanto@gmail.com>
2021-06-26 14:46:20 +0700

    Add package class

---
+++ geometri/triangle.py
@@ -0,0 +1,10 @@
+class Triangle():
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def info(self):
+        return f'This result is {self.width} and {self.height}'
+
+    def count_area(self):
+        return self.width * self.height / 2
